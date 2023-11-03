c161e5a11c369cdb2d8c3a0413f001e630379987
Marcelo Nascimento <marcelohenrique7x@gmail.com>
2023-11-03 04:55:41 -0300

    color current note,

--- lib/modules/play_score/play_score_page.dart
+++ lib/modules/play_score/play_score_page.dart
@@ -6,6 +6,8 @@ import 'package:process_run/process_run.dart';
 import 'package:tutor_musical/modules/share/abc/abc_parser.dart';
 import 'package:tutor_musical/modules/share/ffi/rec_c++.dart';
 
+import '../share/score_element_handler.dart';
+
 class PlayScorePage extends StatefulWidget {
   const PlayScorePage({super.key});
 
@@ -37,14 +39,37 @@ K:G
 
     setState(() {
       score = parse(abcText);
+      handler = ScoreElementHandler(
+        scoreElements: score!,
+        spaceSize: 25,
+        lastLength: length,
+        // initTime: initTime,
+        andamento: andamento,
+      );
+
+      colorChangeStreams = handler!.elements
+          .map((e) => Stream<int>.periodic(
+              Duration(milliseconds: (e.initTime * 1000).toInt()), (j) => j))
+          .toList();
+
+      print(handler?.initTime);
+      for (var e in handler!.elements) {
+        print('${e.initTime} ${e.length}');
+      }
     });
   }
 
   ABCMusic? score;
   double length = 4;
-  double andamento = 60;
+  double andamento = 70;
   ScrollController controller = ScrollController();
   int countdownDuration = 4;
+  bool play = false;
+
+  ScoreElementHandler? handler;
+  List<Stream<int>> colorChangeStreams = [
+    Stream<int>.periodic(Duration(seconds: 1), (j) => j)
+  ];
 
   final rec = Recoder();
   @override
@@ -64,9 +89,9 @@ K:G
           children: [
             IconButton(
               onPressed: () {
-                controller.animateTo(3000,
-                    duration: const Duration(seconds: 100),
-                    curve: Curves.linear);
+                // controller.animateTo(3000,
+                //     duration: const Duration(seconds: 100),
+                //     curve: Curves.linear);
               },
               icon: Icon(
                 Icons.play_arrow_outlined,
@@ -76,9 +101,9 @@ K:G
             ),
             IconButton(
               onPressed: () {
-                controller.animateTo(0,
-                    duration: const Duration(milliseconds: 1),
-                    curve: Curves.linear);
+                // controller.animateTo(0,
+                //     duration: const Duration(milliseconds: 1),
+                //     curve: Curves.linear);
               },
               icon: const Icon(
                 Icons.stop_outlined,
@@ -111,199 +136,26 @@ K:G
                     .copyWith(fontSize: 20),
               ),
               const SizedBox(height: 50),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                controller: controller,
-                child: Row(
-                  children: score != null
-                      ? score!.elements.map((el) {
-                          String s = '';
-                          double topPadding = 0, bottomPadding = 0;
-                          if (el is ABCTone) {
-                            s = '\ue050\ue01a-\ue01a-\ue01a-';
-                          } else if (el is ABCNote) {
-                            String note = '';
+              Wrap(
+                spacing: 0,
+                children: [
+                    if(play)
+                  for (int i = 0; i < handler!.elements.length - 1; i++)
+                    StreamBuilder<int>(
+                      stream: colorChangeStreams[i],
+                      builder: (context, snapshot) {
+                        return handler!.buildRow(context, handler!.elements[i],
+                            color: snapshot.hasData &&
+                                    handler!.elements[i].length != 0
+                                ? Theme.of(context).colorScheme.primary
+                                : null);
+                      },
+                    ),
+                      if(!play)
+                  for (int i = 0; i < handler!.elements.length - 1; i++)
 
-                            switch (el.note) {
-                              case 'A':
-                                topPadding = spaceSize;
-
-                              case 'B':
-                                topPadding = 0;
-
-                              case 'C':
-                                topPadding = 6 * spaceSize;
-
-                              case 'D':
-                                topPadding = 5 * spaceSize;
-
-                              case 'E':
-                                topPadding = 4 * spaceSize;
-
-                              case 'F':
-                                topPadding = 3 * spaceSize;
-
-                              case 'G':
-                                topPadding = 2 * spaceSize;
-
-                              case 'a':
-                                bottomPadding = 6 * spaceSize;
-
-                              case 'b':
-                                bottomPadding = 7 * spaceSize;
-
-                              case 'c':
-                                bottomPadding = 1 * spaceSize;
-                              case 'd':
-                                bottomPadding = 2 * spaceSize;
-
-                              case 'e':
-                                bottomPadding = 3 * spaceSize;
-
-                              case 'f':
-                                bottomPadding = 4 * spaceSize;
-
-                              case 'g':
-                                bottomPadding = 5 * spaceSize;
-                            }
-
-                            double n = length;
-                            if (el.duration != null &&
-                                el.duration!.isNotEmpty) {
-                              for (var e in el.duration!) {
-                                n = e.isMult ? n * 2 : n / 2;
-                              }
-                              // print(n);
-                            }
-                            switch (n) {
-                              case 4:
-                                note = '\uE1D2-';
-                                if (initTime.isNotEmpty) {
-                                  initTime.add(initTime.last + (andamento * 4));
-                                }
-                              case 2:
-                                note = '\uE1D3-';
-                                if (initTime.isNotEmpty) {
-                                  initTime.add(initTime.last + (andamento * 2));
-                                }
-                              case 1:
-                                note = '\uE1D5-';
-                                if (initTime.isNotEmpty) {
-                                  initTime.add(initTime.last + (andamento * 1));
-                                }
-                              case 0.5:
-                                note = '\uE1D7';
-                                if (initTime.isNotEmpty) {
-                                  initTime
-                                      .add(initTime.last + (andamento * 0.5));
-                                }
-                              case 0.25:
-                                note = '\uE1D9';
-                                if (initTime.isNotEmpty) {
-                                  initTime
-                                      .add(initTime.last + (andamento * 0.25));
-                                }
-                            }
-
-                            s = note;
-                          } else if (el is ABCPause) {
-                            String pause = '';
-                            double n = length;
-                            if (el.duration != null &&
-                                el.duration!.isNotEmpty) {
-                              for (var e in el.duration!) {
-                                n = e.isMult ? n * 2 : n / 2;
-                              }
-                              // print(n);
-                            }
-                            switch (n) {
-                              case 4:
-                                pause = '\uE4E3-';
-                                if (initTime.isNotEmpty) {
-                                  initTime.add(initTime.last + (andamento * 4));
-                                }
-                              case 2:
-                                pause = '\uE4E4-';
-                                if (initTime.isNotEmpty) {
-                                  initTime.add(initTime.last + (andamento * 2));
-                                }
-                              case 1:
-                                pause = '\uE4E5-';
-                                if (initTime.isNotEmpty) {
-                                  initTime.add(initTime.last + (andamento * 1));
-                                }
-                              case 0.5:
-                                pause = '\uE4E6-';
-                                if (initTime.isNotEmpty) {
-                                  initTime
-                                      .add(initTime.last + (andamento * 0.5));
-                                }
-                              case 0.25:
-                                pause = '\uE4E7-';
-                                if (initTime.isNotEmpty) {
-                                  initTime
-                                      .add(initTime.last + (andamento * 0.25));
-                                }
-                            }
-
-                            s = '${pause}';
-                          } else if (el is ABCCompasseSeparator) {
-                            s = '\ue030-';
-                          } else if (el is ABCLength) {
-                            length = 4 *
-                                el.length; // length = 1/4 ==> 1/4 de semibreve
-                          }
-
-                          return Row(
-                            children: [
-                              Text(
-                                '\ue020\ue01a',
-                                style: TextStyle(
-                                  fontFamily: 'Bravura',
-                                  fontSize: 5 * spaceSize,
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .onBackground,
-                                  fontFeatures: const [
-                                    FontFeature.enable('liga'),
-                                  ],
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(
-                                    top: topPadding, bottom: bottomPadding),
-                                child: Text(
-                                  s,
-                                  style: TextStyle(
-                                    fontFamily: 'Bravura',
-                                    fontSize: 5 * spaceSize,
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .onBackground,
-                                    fontFeatures: const [
-                                      FontFeature.enable('liga'),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              Text(
-                                '\ue020',
-                                style: TextStyle(
-                                  fontFamily: 'Bravura',
-                                  fontSize: 5 * spaceSize,
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .onBackground,
-                                  fontFeatures: const [
-                                    FontFeature.enable('liga'),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          );
-                        }).toList()
-                      : [],
-                ),
+                      handler!.buildRow(context, handler!.elements[i]),
+                ],
               ),
               FilledButton(
                   onPressed: () async {
@@ -311,7 +163,7 @@ K:G
 
                     countdownDuration = 4;
 
-                    for (; countdownDuration >= 0; countdownDuration--) {
+                    for (; countdownDuration >= 1; countdownDuration--) {
                       showDialog(
                         context: context,
                         builder: (context) {
@@ -324,26 +176,29 @@ K:G
                       Navigator.pop(context);
                     }
 
-                    controller.animateTo(3000,
-                        duration: const Duration(seconds: 100),
-                        curve: Curves.linear);
+                    setState(() {
+                      play = true;
+                    });
 
-                    await compute(rec.recoder, null);
+                    // controller.animateTo(3000,
+                    //     duration: const Duration(seconds: 100),
+                    //     curve: Curves.linear);
 
-                    var shell = Shell();
+                    // await compute(rec.recoder, null);
+
+                    // var shell = Shell();
 
                     // await shell.run(
                     //     'python /home/marcelo/Documents/GitHub/tutor_musical/external/python/getNotes.py /home/marcelo/Documents/GitHub/tutor_musical/assets/rec/gravacao.wav 44100');
 
+                    // await shell.run(
+                    //     'python /home/marcelo/Documents/GitHub/tutor_musical/external/python/notes_process.py');
 
-                    await shell.run(
-                        'python /home/marcelo/Documents/GitHub/tutor_musical/external/python/notes_process.py');
-
-                    print(initTime.map((e) => e / 60).toList());
-                    notes(
-                        initTime.map((e) => e / 60).toList(),
-                        File(
-                            "/home/marcelo/Documents/GitHub/tutor_musical/assets/rec/piano_format.txt"));
+                    // print(initTime.map((e) => e / 60).toList());
+                    // notes(
+                    //     initTime.map((e) => e / 60).toList(),
+                    //     File(
+                    //         "/home/marcelo/Documents/GitHub/tutor_musical/assets/rec/piano_format.txt"));
                   },
                   child: const Text('Gravar'))
             ],
@@ -378,7 +233,8 @@ void notes(List<double> timeIntervals, File arquivo) {
       if (tempoInicial <= limiteFinal && tempoFinal >= limiteInicial) {
         double duracaoNota = tempoFinal - tempoInicial;
 
-        if (!noteDuration.containsKey(nomeNota) || duracaoNota > noteDuration[nomeNota]!) {
+        if (!noteDuration.containsKey(nomeNota) ||
+            duracaoNota > noteDuration[nomeNota]!) {
           noteDuration[nomeNota] = duracaoNota;
         }
       }
